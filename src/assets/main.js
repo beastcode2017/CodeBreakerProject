0fa1428c9d3a2c5f38fd1f906f824e01daa22706
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-04-24 20:26:22 -0500

    Update main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,6 +4,48 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if(answer==='' || attempt==='')
+    {
+      setHiddenFields();
+    }
+
+    if (validateInput(input.value))
+    {
+      attempt+=1;
+    }
+    else {
+      return false;
+    }
 }
 
 //implement new functions here
+function setHiddenFields() {
+  answer = Math.floor(Math.random()*10000);
+  answer = answer.toString();
+  if(answer.length!==4)
+  {
+    while(answer.length<4)
+    {
+      answer=0+answer;
+    }
+  }
+  attempt =0;
+}
+function setMessage(msg) {
+document.getElementById('message').innerHTML(msg);
+}
+
+function validateInput(var1) {
+  if (var1.length === 4)
+  {
+    return true;
+  }
+  else {
+    setMessage("Guesses must be exactly 4 characters long.");
+    return false;
+  }
+}
+
+function getResults(var2) {
+  document.getElementById('results').innerHTML('<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">');
+}
