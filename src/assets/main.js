aeef1b0a7426857dd738831d1d727a6de8079314
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-04-26 10:29:17 -0500

    Still need to fix conditional

--- src/assets/main.js
+++ src/assets/main.js
@@ -23,7 +23,7 @@ function guess() {
       showAnswer(true);
       showReplay();
     }
-    else if ((!getResults)&&(attempt.value>=10)) {
+    else if (attempt.value >= 10) {
       setMessage("You Lose! :(");
       showAnswer(false);
       showReplay();
